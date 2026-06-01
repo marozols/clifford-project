e4e25dc201862a1936abcf00b4f18f9be50f8e4a
Joppe Stokvis <joppe@Joppes-Laptop.local>
2026-06-01 15:23:06 +0200

    added name

--- CliffordProject/Authors.lean
+++ CliffordProject/Authors.lean
@@ -25,3 +25,6 @@ open Informal
 
 :::author "Carli_Bruinsma" (name := "Carli Bruinsma")  (image_url := "https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img,w_2347/https://qusoft.org/wp-content/uploads/2026/02/QuSoft__Carli_Bruinsma_Portrait_MK3_6628.jpg")
 :::
+
+:::author "Joppe Stokvis" (name := "Joppe Stokvis")
+:::
