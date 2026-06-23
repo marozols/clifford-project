7d11763723159cd55fb936dffbddb755ef9285af
Daan Planken <d.p.j.planken@uva.nl>
2026-06-23 16:28:25 +0200

    Add helper lemma for X Z multiplication

--- CliffordProject/Chapters/Pauli.lean
+++ CliffordProject/Chapters/Pauli.lean
@@ -162,3 +162,52 @@ lemma ZX_eq_omega_mul_XZ :
   rw [if_neg h, if_neg h]
 
 ```
+
+And so do their powers.
+
+:::lemma_ "Z_pow_X_pow_eq_omega_mul_X_pow_Z_pow" (parent := "Pauli_core") (owner := "Daan_Planken")
+Pauli $`X` and $`Z` matrices satisfy the following commutation relation:
+$$`Z^k X^ℓ = ω^{k·ℓ} X^ℓ Z^k.`
+:::
+
+```lean "Z_pow_X_pow_eq_omega_mul_X_pow_Z_pow"
+lemma Z_pow_X_pow_eq_omega_mul_X_pow_Z_pow (k : ℕ) (ℓ : ℕ) :
+  (Z d) ^ k * (X d) ^ ℓ = (ω d) ^ (k * ℓ) •
+  ((X d) ^ ℓ * (Z d) ^ k) := by
+    induction ℓ with
+    | zero => simp
+    | succ ℓ ih =>
+      nth_rw 1 [pow_succ']
+      nth_rw 1 [← mul_assoc]
+      have h : Z d ^ k * X d * X d ^ ℓ =  ω d ^ k • X d * Z d ^ k * X d ^ ℓ := by
+        sorry
+      rw [h]
+      nth_rw 1 [mul_assoc]
+      --nth_rw 1 [mul_comm]
+      rw [Matrix.smul_mul]
+      rw [← Matrix.mul_smul]
+      rw [ih]
+      rw [smul_smul]
+      simp
+      rw [← pow_add]
+      rw [← mul_one_add]
+      rw [← mul_assoc]
+      rw [← pow_succ']
+      rw [add_comm]
+
+      --#check Matrix.mul_smul
+      /-rw [smul_mul_assoc]
+      rw [ih]
+      rw [smul_smul]
+      rw [← pow_add]
+      rw [add_comm]
+      rw [← mul_add_one]
+
+      induction k with
+      | zero =>
+        simp
+        rw [pow_succ']
+      | succ k ih2 =>-/
+
+
+```
