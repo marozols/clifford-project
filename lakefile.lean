6cb0f11b28230e2ae835d2b539296abee6821300
Daan Planken <d.p.j.planken@uva.nl>
2026-07-07 15:32:07 +0200

    Set max line limit to 120

--- lakefile.lean
+++ lakefile.lean
@@ -6,7 +6,7 @@ require mathlib from git "https://github.com/leanprover-community/mathlib4"@"v4.
 
 package CliffordProject where
   precompileModules := false
-  leanOptions := #[⟨`experimental.module, true⟩]
+  leanOptions := #[⟨`experimental.module, true⟩, ⟨`verso.code.warnLineLength, 120⟩]
 
 @[default_target]
 lean_lib CliffordProject where
